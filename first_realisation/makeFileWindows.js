cdcc0de083324dba1ee57ac75b8971ad31ebad36
Roman Danilov <r.danylov@gmail.com>
2019-10-29 13:43:50 +0200

    change-windows-autobuild

--- first_realisation/makeFileWindows.js
+++ first_realisation/makeFileWindows.js
@@ -1,27 +1,25 @@
 const execSync = require('child_process').execSync;
-const delay = min => new Promise(res => setTimeout(res, min*60*1000));
 
 execSync('node ../chrysalis-api/build/deleteFile.js');
 process.chdir("../chrysalis-api/");
 execSync('yarn');
-delay(1)
-process.chdir("../chrysalis-api/");
-execSync('node_modules\\.bin\\lerna run build');
+execSync('./node_modules/.bin/lerna run build');
 execSync('node ../chrysalis-api/build/copyFile.js');
 execSync('node ../chrysalis-api/build/renameFile.js');
 process.chdir("../chrysalis");
 execSync('yarn run build:windows');
 
+const delay = min => new Promise(res => setTimeout(res, min*60*1000));
 delay(3)
 
-const distFolder = '..\\chrysalis\\dist\\';
+const distFolder = '../chrysalis/dist/';
 const fs = require('fs');
 let readFile = fs.readdir(distFolder, (err, files) => {
 
   files.forEach(file => {
 
     if (file.slice(-3) === 'exe') {
-      execSync(`..\\chrysalis\\dist\\${file}`);
+      execSync(`../chrysalis/dist/${file}`);
     }
   });
 });
